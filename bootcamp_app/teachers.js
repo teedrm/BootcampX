b78d2d971fdd7cdd7e86b52d4eaf363a3fc44ba5
Thy Nguyen <tee@Thys-iMac.local>
2022-12-15 02:04:53 -0700

    parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name;
+`;
+
+pool.query(queryString, values)
+.then(response => {
+response.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
-  })
 });
+}).catch(err => console.error('query error', err.stack));
